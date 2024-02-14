bc4004b5b8f2ec5a3af3fc8388820b3c880b864d
TravBricks <travis.longwell@databricks.com>
2024-02-14 18:20:04 -0500

    Actually delete the cluster!

--- cluster_testing_cleanup.py
+++ cluster_testing_cleanup.py
@@ -6,3 +6,4 @@ for c in w.clusters.list():
   if 'Standard_' in c.cluster_name:
      print(f"Deleting '{c.cluster_name}' id={c.cluster_id}")
      w.clusters.delete(cluster_id=c.cluster_id).result()
+     w.clusters.permanent_delete(cluster_id=c.cluster_id)
